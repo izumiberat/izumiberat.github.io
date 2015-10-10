69448994e7bac400e273c72fd93026d722e40415
izumiberat <igb2107@columbia.edu>
2015-10-09 21:02:39 -0400

    Update alumnitracking.js

--- alumnitracking.js
+++ alumnitracking.js
@@ -1,3 +1,32 @@
+<!-- Code for Action: 9.17.15_Columbia EDU Universal Pixel -->
+<!-- Begin Rocket Fuel Conversion Action Tracking Code Version 9 -->
+<script type='text/javascript'>
+(function() {
+	var w = window, d = document;
+	var s = d.createElement('script');
+	s.setAttribute('async', 'true');
+	s.setAttribute('type', 'text/javascript');
+	s.setAttribute('src', '//c1.rfihub.net/js/tc.min.js');
+	var f = d.getElementsByTagName('script')[0];
+	f.parentNode.insertBefore(s, f);
+	if (typeof w['_rfi'] !== 'function') {
+		w['_rfi']=function() {
+			w['_rfi'].commands = w['_rfi'].commands || [];
+			w['_rfi'].commands.push(arguments);
+		};
+	}
+	_rfi('setArgs', 'ver', '9');
+	_rfi('setArgs', 'rb', '6659');
+	_rfi('setArgs', 'ca', '20719914');
+	_rfi('track');
+})();
+</script>
+<noscript>
+  <iframe src='//20719914p.rfihub.com/ca.html?rb=6659&ca=20719914&ra=%n' style='display:none;padding:0;margin:0' width='0' height='0'>
+</iframe>
+</noscript>
+<!-- End Rocket Fuel Conversion Action Tracking Code Version 9 -->
+
 <script type="text/javascript">
   _bizo_data_partner_id = "8878";
 </script>
